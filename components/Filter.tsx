0bb090e40d292537391d719b8a043c0bca49c6ca
Yogesh Shahi <yogeshshahi120@gmail.com>
2024-12-08 07:09:07 +0530

    chore: code clean up and deployment fixes

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -16,46 +16,43 @@ export const Filter: React.FC<Props> = ({
   onChange,
 }) => {
   return (
-    <>
-      <Menu as="div" className="relative inline-block text-left">
-        <div>
-          <Menu.Button className="appearance-none w-full flex flex-row md:w-48 px-4 py-2 bg-gray-800/50 border border-gray-700 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-yellow-500">
-            {category}: {activeItem}
-            <ChevronDownIcon
-              className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
-              aria-hidden="true"
-            />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="z-50 absolute left-0 w-56 mt-2 origin-top-left bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="px-2 py-1">
-              {list.map((item) => (
-                <Menu.Item key={item} onClick={() => onChange(item)}>
-                  {({ active }) => {
-                    return (
-                      <button
-                        className={`${activeItem === item && "bg-gray-200"
-                          } font-medium group flex rounded-md items-center w-full px-3 py-2 text-sm text-gray-900 hover:bg-gray-200 my-1`}
-                      >
-                        {item}
-                      </button>
-                    );
-                  }}
-                </Menu.Item>
-              ))}
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </>
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="appearance-none w-full flex flex-row md:w-48 px-4 py-2 bg-gray-800/50 border border-gray-700 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-yellow-500">
+          {category}: {activeItem}
+          <ChevronDownIcon
+            className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="z-50 absolute left-0 w-56 mt-2 origin-top-left bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="px-2 py-1">
+            {list.map((item) => (
+              <Menu.Item key={item} as={Fragment}>
+                {({ active }) => (
+                  <button
+                    onClick={() => onChange(item)}
+                    className={`${activeItem === item ? "bg-gray-200" : ""} 
+                      font-medium group flex rounded-md items-center w-full px-3 py-2 text-sm text-gray-900 hover:bg-gray-200 my-1`}
+                  >
+                    {item}
+                  </button>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 };
